4b98aea502cd4035245710a2ac15d358acc018d5
caramelmelmel <melody_leong@mymail.sutd.edu.sg>
2021-08-07 15:54:16 +0800

    app.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react'
 import { BrowserRouter as Router, Route} from "react-router-dom";
 
 //import internal components here
+import {Login} from './pages/login';
 import './App.css';
 
 
 function App() {
   return (
     <Router>
-      <Route exact path='/login' component={}/>
+      <Route exact path='/login' component={Login}/>
       <Route exact path='/register' component={}/>
     </Router>
   );
